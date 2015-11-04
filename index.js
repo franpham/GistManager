d37b3380a71bd8d788758c719ea939ec2cc9dd50
kasi <kasionaga@gmail.com>
2015-11-03 15:34:58 -1000

    Got Github authorization login to work

--- index.js
+++ index.js
@@ -30,6 +30,51 @@ app.use(express.static('./public'));
  */
 app.use(bodyParser.urlencoded({ extended : true }));
 
+/**
+ * Step 1 : Oauth2, getting hte provider's auth url
+ */
+app.get('/auth/login', (req, res ) => {     // route handler
+  var authURL = oauth2.getAuthorizeUrl({
+    redirect_uri : 'http://localhost:3000/auth/github/callback',
+    scope : ['gist'], // asking users for persmission
+    state : 'Authorize' + Math.round(Math.random() * 9999999)
+  });
+  res.json({ url : authURL }); // Gives url to user
+});
+
+/**
+ * Step 2 : Callback from provider, with code, on succesful authorization
+ * this route must be set _exactly_ as it is set on the provider
+ * as callback url
+ * - use the code, to exchange for an access_token
+ */
+app.get('/auth/github/callback', (req, res) => {
+  var code = req.query.code;
+
+  if(code === undefined) {
+    return res.status(401).json({ error : 401, message : 'Invalid auth code' });
+  }
+  oauth2.getOAuthAccessToken(
+    code,
+    {
+      redirect_uri : 'http://localhost:3000/auth/github/callback'
+    },
+    (err, access_token, refresh_token, results) => {
+      if (err) {
+        console.error(err);
+        res.status(401).json(err);
+      } else if ( results.error ) {
+        console.error(results.error);
+        res.status(401).json(results.error);
+      } else { // everything worked
+        // get token
+        //    send token back to client
+        res.json({ access_token : access_token });
+      }
+    }
+    );
+});
+
 
 /**
  * AuthBearerToken
